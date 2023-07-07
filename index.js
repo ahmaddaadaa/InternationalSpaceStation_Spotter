11fb93d24b11a9f7a8b87b6c1d7b12d76ef28e59
ahmaddaadaa <ahmadd@uvic.ca>
2023-07-06 17:03:36 -0700

    updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes,nextISSTimesForMyLocation } = require('./iss');
 
 
 fetchMyIP((error, ip) => {
@@ -7,5 +7,37 @@ fetchMyIP((error, ip) => {
     return;
   }
   console.log('It worked! Returned IP:', ip);
-});
 
+  fetchCoordsByIP(ip, (error, data) => {
+    if (error) {
+      console.log('fetchCoordsByIP Error:', error);
+    } else {
+      console.log('Coordinates:', data);
+      fetchISSFlyOverTimes(data, (error, flyover) => {
+        if (error) {
+          console.log('fetchISSFlyOverTimes Error:', error);
+        } else {
+          console.log('flyover:', flyover);
+        }});
+    }
+  });
+
+
+  const printPassTimes = function(passTimes) {
+    for (const pass of passTimes) {
+      const datetime = new Date(0);
+      datetime.setUTCSeconds(pass.risetime);
+      const duration = pass.duration;
+      console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    }
+  };
+  
+  nextISSTimesForMyLocation((error, passTimes) => {
+    if (error) {
+      return console.log("It didn't work!", error);
+    }
+    // success, print out the deets!
+    printPassTimes(passTimes);
+  });
+
+});
